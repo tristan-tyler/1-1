d6a8918b857f35c6427f0670c107436fc2010792
Tristan Tyler <gr8engineer2b@gmail.com>
2025-03-31 12:27:12 -0400

    movement tweaks added directional friction offset to make controls less sluggish added killing y velocity on collision player collision

--- player.gd
+++ player.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
 
-var speed = 400
+var speed = 200
 var jump_speed = 12000
 var can_jump = true
 var jump_max = 80
 var gravity = 800
-var friction = 0.05
+var friction = 0.1
 
 func _physics_process(delta: float) -> void:
-	velocity.x += velocity.x * -friction
+	var friction_offset = velocity.x * -friction
 	if Input.is_action_pressed("ui_left"):
 		velocity += Vector2.LEFT * delta * speed
+		velocity += Vector2.LEFT * abs(friction_offset) # offset friction during movement
 	elif Input.is_action_pressed("ui_right"):
 		velocity += Vector2.RIGHT * delta * speed
+		velocity += Vector2.RIGHT * abs(friction_offset) # offset friction during movement
+	velocity.x += friction_offset
 	if Input.is_action_pressed("ui_up") and can_jump:
 		if position.y < -jump_max:
 			can_jump = false
@@ -26,10 +29,11 @@ func _physics_process(delta: float) -> void:
 		velocity += -collision.get_remainder()
 		if is_on_floor():
 			can_jump = true
+			velocity = Vector2(velocity.x, 0) # prevents animation glitches due to multiple collisions
 	else:
 		velocity += Vector2.DOWN * delta * gravity
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var input_animation = "big_idle"
 	if Input.is_action_just_pressed("ui_left"):
 		$"AnimatedSprite2D".flip_h = false
