22d41dddb18bd9b6c3bec67f2098bc58cf357d93
Tristan Tyler <gr8engineer2b@gmail.com>
2025-03-31 11:52:17 -0400

    changed project structure added layer physics added player animations

--- player.gd
+++ player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-var speed = 1000
-var jump_speed = 30000
+var speed = 400
+var jump_speed = 12000
 var can_jump = true
-var jump_max = 200
-var gravity = 1200
-var friction = 0.035
+var jump_max = 80
+var gravity = 800
+var friction = 0.05
 
-func _physics_process(delta: float) -> void:		
+func _physics_process(delta: float) -> void:
 	velocity.x += velocity.x * -friction
 	if Input.is_action_pressed("ui_left"):
 		velocity += Vector2.LEFT * delta * speed
@@ -28,3 +28,18 @@ func _physics_process(delta: float) -> void:
 			can_jump = true
 	else:
 		velocity += Vector2.DOWN * delta * gravity
+
+func _process(delta: float) -> void:
+	var input_animation = "big_idle"
+	if Input.is_action_just_pressed("ui_left"):
+		$"AnimatedSprite2D".flip_h = false
+	elif Input.is_action_just_pressed("ui_right"):
+		$"AnimatedSprite2D".flip_h = true
+	if not is_on_floor():
+		input_animation = "big_jump"
+	elif Input.is_action_pressed("ui_down"):
+		input_animation = "crouch"
+	elif abs(velocity.x) > speed * friction:
+		input_animation = "big_run"
+		
+	$"AnimatedSprite2D".play(input_animation)
