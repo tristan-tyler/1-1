11a944c9485b261e5a1f6f92bda68dc5a61eafc2
Tristan Tyler <gr8engineer2b@gmail.com>
2025-03-31 12:55:06 -0400

    fixed jump being based on absolute position

--- player.gd
+++ player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 var speed = 200
 var jump_speed = 12000
-var can_jump = true
+var jump = true
+var jump_start = position
 var jump_max = 80
 var gravity = 800
 var friction = 0.1
+
 
 func _physics_process(delta: float) -> void:
 	var friction_offset = velocity.x * -friction
@@ -16,19 +18,20 @@ func _physics_process(delta: float) -> void:
 		velocity += Vector2.RIGHT * delta * speed
 		velocity += Vector2.RIGHT * abs(friction_offset) # offset friction during movement
 	velocity.x += friction_offset
-	if Input.is_action_pressed("ui_up") and can_jump:
-		if position.y < -jump_max:
-			can_jump = false
+	if Input.is_action_pressed("ui_up") and jump:
+		if jump_start.y - jump_max > position.y:
+			jump = false
 		else:
 			velocity.y = (Vector2.UP * delta * jump_speed).y
 	else:
-		can_jump = false
+		jump = false
 
 	if move_and_slide():
 		var collision = get_last_slide_collision()
 		velocity += -collision.get_remainder()
 		if is_on_floor():
-			can_jump = true
+			jump = true
+			jump_start = position
 			velocity = Vector2(velocity.x, 0) # prevents animation glitches due to multiple collisions
 	else:
 		velocity += Vector2.DOWN * delta * gravity
